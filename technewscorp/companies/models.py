444cf6ced1d82aa3bac5de3fca447927fec03e06
Carlos Garcia <carlos.grh@outlook.com>
2022-10-31 16:17:39 -0500

    create django project for company hiring status, create company model and register on admin.py

---
+++ technewscorp/companies/models.py
@@ -0,0 +1,22 @@
+from email.policy import default
+from random import choices
+from django.db import models
+from django.utils.timezone import now
+from django.db.models import URLField
+
+# Create your models here.
+
+
+class Company(models.Model):
+
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = "Layoffs"
+        HIRING_FREEZE = "Hiring Freeze"
+        HIRING = "Hiring"
+
+
+    name = models.CharField(max_length=30, unique=True)
+    status = models.CharField(choices=CompanyStatus.choices, default=CompanyStatus.HIRING, max_length=30)
+    last_update = models.DateTimeField(default=now, editable=True)
+    application_linl = URLField(blank=True)
+    notes = models.CharField(max_length=100, blank=True)
