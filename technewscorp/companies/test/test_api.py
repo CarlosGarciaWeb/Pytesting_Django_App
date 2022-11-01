637c505f63e7b4df903dbd199bd8be98090d9539
Carlos Garcia <carlos.grh@outlook.com>
2022-11-01 11:19:52 -0500

    create test_api.py and add a unittest for empty list of companies

---
+++ technewscorp/companies/test/test_api.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from django.test import Client
+from django.urls import reverse
+import json
+import pytest
+
+@pytest.mark.django_db
+class TestGetCompanies(TestCase):
+    def test_zero_companies_should_return_empty_list(self) -> None:
+        client = Client()
+        companies_url = reverse("companies-list")
+        response = client.get(companies_url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.loads(response.content), [])
--- test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-
-def test_fomula() -> None:
-    assert 1 == 2
-
