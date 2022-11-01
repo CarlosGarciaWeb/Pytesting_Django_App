8f23d96f80e2370dd60510264861c5726126fd62
Carlos Garcia <carlos.grh@outlook.com>
2022-11-01 12:20:27 -0500

    write function to test empty database and get a particular company name, created setUp function to avoid code duplication

--- technewscorp/companies/test/test_api.py
+++ technewscorp/companies/test/test_api.py
@@ -1,14 +1,29 @@
 from unittest import TestCase
 from django.test import Client
 from django.urls import reverse
+from companies.models import Company
 import json
 import pytest
 
 @pytest.mark.django_db(transaction=True)
 class TestGetCompanies(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.companies_url = reverse("companies-list")
+
     def test_zero_companies_should_return_empty_list(self) -> None:
-        client = Client()
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
-        self.assertEqual(response.status_code, 400)
+        response = self.client.get(self.companies_url)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
+
+
+    def test_one_company_should_succeed(self) -> None:
+        test_company = Company.objects.create(name="Amazon")
+        response = self.client.get(self.companies_url)
+        response_content = json.loads(response.content)[0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_content.get("name"), test_company.name)
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+
+        test_company.delete()
