cbee556b1e59b9439df3fefe45569d7f83c79901
Carlos Garcia <carlos.grh@outlook.com>
2022-11-02 11:49:34 -0500

    create send email function in views.py and connect to urls.py also create settings for send email function django.

--- technewscorp/companies/views.py
+++ technewscorp/companies/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
 from rest_framework.viewsets import ModelViewSet
-from .serializers import CompanySerializer
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import api_view
+from .serializers import CompanySerializer
 from .models import Company
 
 # Create your views here.
@@ -11,3 +13,23 @@ class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by("-last_update")
     pagination_class = PageNumberPagination
+
+
+@api_view(http_method_names=['POST'])
+def send_company_email(request):
+    """
+    
+        sends email with request payload
+        sender: someone@email.com
+        receiver: othersomeone@email.com
+
+
+    """
+
+    send_mail (
+        'Subject here', 'Here is the message.',
+        'carlos.grh@outlook.com', ['carlos.grh@outlook.com'],
+        fail_silently=False
+    )    
+
+    return Response({"status": "success", "info": "email sent successfully"}, status=200)
--- technewscorp/technewscorp/urls.py
+++ technewscorp/technewscorp/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from companies.views import send_company_email
 from companies.urls import companies_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(companies_router.urls)),
+    path("send-email", send_company_email)
 ]
