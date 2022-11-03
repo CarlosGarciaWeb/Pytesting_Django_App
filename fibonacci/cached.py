a411fec41086bba072734c45ce46b1aeff8a7254
Carlos Garcia <carlos.grh@outlook.com>
2022-11-03 10:59:01 -0500

    add dynamic programming function for fibonacci function.

--- fibonacci/cached.py
+++ fibonacci/cached.py
@@ -2,15 +2,15 @@ from functools import lru_cache
 
 cache = {}
 
+
 def fibonacci_cached(n: int) -> int:
     if n in cache:
         return cache[n]
     if n == 0 or n == 1:
         return n
-    fn = fibonacci_cached(n -1) + fibonacci_cached(n - 2)
+    fn = fibonacci_cached(n - 1) + fibonacci_cached(n - 2)
     cache[n] = fn
     return fn
-
 
 
 @lru_cache(maxsize=256)
--- fibonacci/tests/test_fibonacci_func.py
+++ fibonacci/tests/test_fibonacci_func.py
@@ -3,12 +3,13 @@ import pytest
 from ..cached import fibonacci_cached, fibonacci_lru_cached
 from typing import Callable
 from fixtures import time_tracker
+from ..dynamic import fibonacci_dynamic, fibonacci_dynamic_On
+
 
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_naive(n: int, expected: int) -> None:
     res = naive.fibonacci_naive(n=n)
     assert res == expected
-
 
 
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
@@ -17,8 +18,12 @@ def test_cached(n: int, expected: int) -> None:
     assert res == expected
 
 
-@pytest.mark.parametrize("fib_func", [naive.fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
-@pytest.mark.parametrize("n, expected", [(0,0),(1,1),(2,1),(20, 6765)])
-def test_fibonnaci_funcs(time_tracker, fib_func:Callable[[int], int], n: int, expected: int) -> None:
+@pytest.mark.parametrize(
+    "fib_func", [naive.fibonacci_naive, fibonacci_cached, fibonacci_lru_cached, fibonacci_dynamic, fibonacci_dynamic_On]
+)
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonnaci_funcs(
+    time_tracker, fib_func: Callable[[int], int], n: int, expected: int
+) -> None:
     res = fib_func(n)
     assert res == expected
