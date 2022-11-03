12141b62acdd1d8005cc3cfbaa72d69061a9fbd4
Carlos Garcia <carlos.grh@outlook.com>
2022-11-03 14:04:36 -0500

    add track performace so that test fails upon timing out or in other words exceeding the time established on execution

--- fibonacci/tests/test_performance.py
+++ fibonacci/tests/test_performance.py
@@ -1,6 +1,9 @@
 from fibonacci.dynamic import fibonacci_dynamic_On
 import pytest
+from fixtures import track_performance
+from time import sleep
 
 @pytest.mark.performance
+@track_performance
 def test_performance():
     fibonacci_dynamic_On(1000)
--- fixtures.py
+++ fixtures.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytest
+from typing import Callable
 
 @pytest.fixture
 def time_tracker():
@@ -8,3 +9,29 @@ def time_tracker():
     end_time = datetime.now()
     time_delta = end_time - start_time
     print(f"\nruntime: {time_delta.total_seconds()}s")
+
+
+class PerformanceException(Exception):
+    def __init__(self, runtime: timedelta, limit: timedelta):
+        self.runtime = runtime
+        self.limit = limit
+
+    def __str__(self) -> str:
+        return f"Performance test failed, runtime: {self.runtime.total_seconds()}, limit: {self.limit.total_seconds()}"
+
+
+
+def track_performance(method: Callable, runtime_limit=timedelta(seconds=2)):
+    def run_function_and_validate_runtime(*args, **kw):
+        start_time = datetime.now()
+        result = method(*args, **kw)
+        end_time = datetime.now()
+        time_delta = end_time - start_time
+        print(f"\nruntime: {time_delta.total_seconds()}s")
+
+        if time_delta > runtime_limit:
+            raise PerformanceException(runtime=time_delta, limit=runtime_limit)
+
+        return result
+
+    return run_function_and_validate_runtime
